c9e35bd6de91dc69e591143aabeb0984a22ce478
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-12-17 13:27:52 -0800

    render in test seems to be broken but I wrote the tests blind about as best I could

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -5,19 +5,47 @@ import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
-import { render } from '@testing-library/react';
+import { render } from 'express/lib/response';
+
+const testArticle = {
+    id:"0",
+    headline: "testHeadline",
+    author: "",
+    summary: "testSummary",
+    body: "testBody"
+};
+
 
 test('renders component without errors', ()=> {
+    render(<Article />);
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+test('renders headline, author from the article when passed in through props', ()=> {
+    render(<Article />);
+    const headline = screen.queryByTestId('headline');
+    const author = screen.queryByTestId('author');
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+    expect(headline).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
+});
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+test('renders "Associated Press" when no author is given', ()=> {
+    render(<Article article={testArticle} />);
+    const author = screen.queryByTestId('author');
 
-//Task List:
-//1. Complete all above tests. Create test article data when needed.
+    expect(author).toHaveTextContent('Associated Press');
+});
+
+test('executes handleDelete when the delete button is pressed', async ()=> {
+    const testHandleDelete = jest.fn();
+
+    render(<Article handleDelete={testHandleDelete} />);
+
+    const deleteButton = screen.queryByTestId('deleteButton');
+    userEvent.click(deleteButton);
+
+    await waitFor(() => expect(testHandleDelete).toBeCalled());
+});
+
+// Task List:
+// 1. Complete all above tests. Create test article data when needed.
